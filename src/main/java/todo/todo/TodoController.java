c54950db5d2763df9359386d23bf06bdba87f287
sooshiance <s.katafract@gmail.com>
2024-09-21 22:50:19 +0330

    feat: Update & Delete functionalities added

--- src/main/java/todo/todo/TodoController.java
+++ src/main/java/todo/todo/TodoController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/todo")
@@ -22,5 +25,31 @@ public class TodoController {
         return todoItemRepository.save(item);
     }
 
-    // Implement update and delete endpoints as needed
+    @PutMapping("/{id}")
+    public ResponseEntity<TodoItem> updateItem(@PathVariable Long id, @RequestBody TodoItem itemDetails) {
+        Optional<TodoItem> optionalTodo = todoItemRepository.findById(id);
+
+        if (optionalTodo.isPresent()) {
+            TodoItem existingTodo = optionalTodo.get();
+            existingTodo.setTitle(itemDetails.getTitle());
+            existingTodo.setCompleted(itemDetails.isCompleted());
+
+            final TodoItem updatedTodo = todoItemRepository.save(existingTodo);
+            return ResponseEntity.ok(updatedTodo);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteItem(@PathVariable Long id) {
+        Optional<TodoItem> optionalTodo = todoItemRepository.findById(id);
+
+        if (optionalTodo.isPresent()) {
+            todoItemRepository.delete(optionalTodo.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
